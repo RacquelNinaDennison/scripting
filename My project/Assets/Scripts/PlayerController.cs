d5bb08501a2384b28d0b373431006f2bc87774b3
RacquelNinaDennison <racquelnina@gmail.com>
2024-01-03 15:26:48 +0200

    Added the controller script to move the ball

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+
+
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    // Speed at which the player moves.
+    public float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement*speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue movementValue) {
+        // this will set the movement vector that will assist the users move around
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+        
+
+    }
+
+}
